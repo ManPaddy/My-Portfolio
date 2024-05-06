21a4e869cfae983f032949d9a8abc5d26bd9ec4d
ManPaddy <onahfavour@gmail.com>
2024-05-06 19:39:14 +0100

    My Commits

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+export default function AboutMe() {
+    return (
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1 className="skills--section--heading">About Me</h1>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iusto sunt illo magnam officia dolorem aut saepe totam quod temporibus nobis itaque praesentium autem consectetur nihil ipsam alias, perferendis tempora.</p>
+                    <p className="hero--section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde iusto sunt illo magnam officia dolorem aut saepe totam quod temporibus.</p>
+                </div>
+                
+            </div>
+        </section>
+    )
+}
